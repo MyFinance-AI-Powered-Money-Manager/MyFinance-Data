c8a84ac29f097cd394cd67bd45a1bfa6a989558c
Hinjikani <pascalhanif@gmail.com>
2026-04-21 16:14:46 +0700

    Add product scraping from each subcategory

--- alfamart-scrape.py
+++ alfamart-scrape.py
@@ -1,5 +1,25 @@
 from playwright.sync_api import sync_playwright
 import pandas as pd
+import time
+
+def get_products(browser, page, subcategory_link):
+    product_list = []
+    page.goto(subcategory_link)
+    while True:
+        page.wait_for_selector(".mb-0.px-2.product_name.text-default", timeout=10000)
+        products = page.locator(".mb-0.px-2.product_name.text-default").all()
+        for product in products:
+            product_name = product.inner_text().strip()
+            product_list.append(product_name)
+        next_button = page.get_by_label("Go to next page")
+        is_disabled = next_button.get_attribute("aria-disabled")
+
+        if is_disabled == None:
+            next_button.click()
+        else:
+            break
+    
+    return(product_list)
 
 def main():
     data = []
@@ -17,14 +37,21 @@ def main():
             subcategories = parent.locator(".card.submenu li a").all()
             for subcategory in subcategories:
                 subcategory_name = subcategory.inner_text().strip()
-                data.append({
-                    "Category": category_name,
-                    "Subcategory": subcategory_name
-                })
+                if subcategory_name == "Rokok & Korek":
+                    continue
+                subcategory_link = subcategory.get_attribute("href")
+                products = get_products(browser, page, f"https://alfagift.id{subcategory_link}")
+                for product in products:
+                    data.append({
+                        "Category": category_name,
+                        "Subcategory": subcategory_name,
+                        "product": product
+                    })
+                time.sleep(2)
             
         
         df = pd.DataFrame(data)
-        print(df)
+        df.to_csv("alfagift_scrape.csv")
         browser.close()
 
 if __name__ == "__main__":
