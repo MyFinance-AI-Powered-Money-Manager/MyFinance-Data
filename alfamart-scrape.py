3424082869fe8436c01e0d15c906954c975cd747
Hinjikani <pascalhanif@gmail.com>
2026-04-21 13:25:07 +0700

    Add looping through every category and subcategory

--- alfamart-scrape.py
+++ alfamart-scrape.py
@@ -2,15 +2,28 @@ from playwright.sync_api import sync_playwright
 import pandas as pd
 
 def main():
+    data = []
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://alfagift.id")
-        # Mengambil lokasi dari html element dengan class list-group-item dan list-1v1 
-        # Setelahnya mencari span yang terdapat didalam element a
-        category_locator = page.locator(".list-group-item.list-lv1 > a span")
-        category = category_locator.all_inner_texts()
-        df = pd.DataFrame({"category": category})
+        # Mengambil content didalam element dengan class list-group-item dan list-lv1
+        parent_containers = page.locator(".list-group-item.list-lv1").all()
+
+        for parent in parent_containers:
+            # Mengambil text didalam element span yang berada didalam element a
+            category_name = parent.locator("> a span").inner_text().strip()
+            # Mengambil content a didalam element li didalam element dengan class card dan sebmenu
+            subcategories = parent.locator(".card.submenu li a").all()
+            for subcategory in subcategories:
+                subcategory_name = subcategory.inner_text().strip()
+                data.append({
+                    "Category": category_name,
+                    "Subcategory": subcategory_name
+                })
+            
+        
+        df = pd.DataFrame(data)
         print(df)
         browser.close()
 
